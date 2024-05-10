0b1fd74fa49affd37ae623ff9dd5252a11f75ed6
Scott Quested <me@scottquested.com>
2024-05-10 21:53:49 +0100

    feat: Add cookie bot banner

---
+++ src/components/CookieBot.tsx
@@ -0,0 +1,13 @@
+import Script from "next/script";
+
+export async function CookiebotBanner() {
+  return (
+    <Script
+      id="Cookiebot"
+      src="https://consent.cookiebot.com/uc.js"
+      data-cbid="67b29a32-f6f7-4613-96ec-d2be109f42ed"
+      data-blockingmode="auto"
+      type="text/javascript"
+    />
+  );
+}
